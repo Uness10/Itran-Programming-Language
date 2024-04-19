a1441eca3adb0f244a515f28a139c1344494c326
Uness10 <younessanouar57@gmail.com>
2024-04-19 17:56:58 +0100

    Initial commit

---
+++ build/CMakeFiles/itran.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/itran.dir/src/main.cpp.obj"
+  "CMakeFiles/itran.dir/src/main.cpp.obj.d"
+  "itran.exe"
+  "itran.exe.manifest"
+  "itran.pdb"
+  "libitran.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/itran.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
